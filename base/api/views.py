246579bc516bf8d74a7d06386735d0b514499f9e
Artem Kindiak <artiomkindiak@gmail.com>
2022-08-15 09:59:50 +0300

    add register user flow, add category, product models

--- base/api/views.py
+++ base/api/views.py
@@ -1,4 +1,11 @@
 from django.http import JsonResponse
+
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+
+from .serializers import RegisterSerializer
+from rest_framework import generics
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -16,3 +23,9 @@ def getRoutes(request):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
--- base/models.py
+++ base/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField('category name', max_length=255)
+    description = models.TextField('category description')
+    code = models.CharField('category code', max_length=255, blank=True, null=True)
+    parent_id = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField('product name', max_length=255)
+    description = models.TextField('description')
+    code = models.CharField('product code', max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    price = models.DecimalField('product price', decimal_places=2, max_digits=10)
+    quantity = models.IntegerField('product quantity', default=0)
+    brand = models.CharField('product brand', max_length=255)
+    category_id = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+    is_available = models.BooleanField('product availability', default=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.name
