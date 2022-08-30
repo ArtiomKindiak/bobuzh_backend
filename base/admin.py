ae6938213b3f0da975ab19a14e5b8ddfd20a929c
Artem Kindiak <artiomkindiak@gmail.com>
2022-08-30 09:29:31 +0300

    Add new models: Order, Customer, OrderItem. Register them in admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Order, OrderItem, Customer
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        total = obj.quantity * obj.product.price
+        obj.product.quantity -= obj.quantity
+        obj.order.total_price += total
+        obj.order.products.add(obj.product)
+        obj.order.save()
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem, OrderItemAdmin)
--- base/models.py
+++ base/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -36,3 +41,34 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    email = models.EmailField('email', blank=True, null=True)
+    first_name = models.CharField('first name', max_length=255)
+    last_name = models.CharField('last name', max_length=255)
+    mobile_number = models.CharField('mobile number', max_length=50)
+    address = models.CharField('address', max_length=255)
+    post_address = models.CharField('post address', max_length=255)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True, null=True)
+    products = models.ManyToManyField(Product, blank=True)
+    total_price = models.DecimalField('total order price', default=0.00, max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField('product quantity', default=1)
+
+
+@receiver(post_save, sender=OrderItem)
+def update_order(sender, instance, **kwargs):
+    total = instance.quantity * instance.product.price
+    instance.product.quantity -= instance.quantity
+    instance.order.total_price += total
+    instance.order.products.add(instance.product)
